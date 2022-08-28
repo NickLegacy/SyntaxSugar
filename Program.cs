cf2db54357f9067c0eb456c0d9be9d367cd31889
NickLegacy <nicklegacy13@gmail.com>
2022-08-28 16:47:30 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SyntaxSugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer < 9) ? "Is less than nine" : "greater than or equal to nine";
+            Console.WriteLine (response);
+        
+        }
+    }
+}
